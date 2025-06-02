87618e90b5cfec09fd9ab37fde6364af75ee164d
zahidhasannipu01 <zahidhasannipu01@gmail.com>
2025-06-02 19:54:04 +0600

    Complete Landing Page but not responsive yet

--- app/components/footers/Footer.jsx
+++ app/components/footers/Footer.jsx
@@ -1,12 +1,31 @@
 import Container from "@/app/utils/Container";
 import Link from "next/link";
 import React from "react";
+import Logo from "../../../public/assets/logo/logo.svg";
+import Image from "next/image";
+import Facebook from "../../../public/assets/icon/facebook.svg";
+import Twitter from "../../../public/assets/icon/twitter.svg";
+import LinkedIn from "../../../public/assets/icon/linkedin.svg";
 const Footer = () => {
   return (
     <Container>
       <div className="flex justify-between py-5">
-        <div>
-          <p>Logo and Description</p>
+        <div className="flex flex-col gap-y-5">
+          <div>
+            <Image src={Logo} alt="logo" />
+          </div>
+          <div className="text-[#6B7280] text-sm text-wrap w-[400px] leading-5">
+            <p>
+              Leading digital agency with solid design and development
+              expertise. We build readymade websites, mobile applications, and
+              elaborate online business services.
+            </p>
+          </div>
+          <div className="flex gap-x-5">
+            <Image src={Facebook} className="cursor-pointer" alt="facebook" />
+            <Image src={Twitter} className="cursor-pointer" alt="twitter" />
+            <Image src={LinkedIn} className="cursor-pointer" alt="linkedin" />
+          </div>
         </div>
         <div className="">
           <div className="grid grid-cols-4 gap-x-32 ">
@@ -28,53 +47,41 @@ const Footer = () => {
               </div>
             </div>
             <div>
-              <p className="text-xl font-bold">What We Do</p>
+              <p className="text-xl font-bold">Company</p>
               <div className="gap-y-2 flex flex-col pt-4 text-sm">
                 <Link href="#">
-                  <p className="">Web Design</p>
+                  <p className="">About Us</p>
                 </Link>
                 <Link href="#">
-                  <p className="">App Design</p>
+                  <p className="">Career</p>
                 </Link>
                 <Link href="#">
-                  <p className="">Social Media Manage</p>
-                </Link>
-                <Link href="#">
-                  <p className="">Market Analysis Project</p>
+                  <p className="">Become Investor</p>
                 </Link>
               </div>
             </div>
             <div>
-              <p className="text-xl font-bold">What We Do</p>
+              <p className="text-xl font-bold">Support</p>
               <div className="gap-y-2 flex flex-col pt-4 text-sm">
                 <Link href="#">
-                  <p className="">Web Design</p>
+                  <p className="">FAQ</p>
                 </Link>
                 <Link href="#">
-                  <p className="">App Design</p>
+                  <p className="">Policy</p>
                 </Link>
                 <Link href="#">
-                  <p className="">Social Media Manage</p>
-                </Link>
-                <Link href="#">
-                  <p className="">Market Analysis Project</p>
+                  <p className="">Business</p>
                 </Link>
               </div>
             </div>
             <div>
-              <p className="text-xl font-bold">What We Do</p>
+              <p className="text-xl font-bold">Contact</p>
               <div className="gap-y-2 flex flex-col pt-4 text-sm">
                 <Link href="#">
-                  <p className="">Web Design</p>
+                  <p className="">WhatsApp</p>
                 </Link>
                 <Link href="#">
-                  <p className="">App Design</p>
-                </Link>
-                <Link href="#">
-                  <p className="">Social Media Manage</p>
-                </Link>
-                <Link href="#">
-                  <p className="">Market Analysis Project</p>
+                  <p className="">Support 24</p>
                 </Link>
               </div>
             </div>
